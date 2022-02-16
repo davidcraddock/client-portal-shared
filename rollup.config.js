d8ccf6503e161697b96a1a78a0191c6a0bc36f90
david craddock <davidcraddock@users.noreply.github.com>
2022-02-17 10:23:11 +1100

    Add files via upload

--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,12 @@ import resolve from '@rollup/plugin-node-resolve'
 import external from 'rollup-plugin-peer-deps-external'
 import commonjs from '@rollup/plugin-commonjs'
 import typescript from 'rollup-plugin-typescript2'
+import dts from "rollup-plugin-dts";
 import svgr from '@svgr/rollup'
 import packageJson from './package.json'
 
-export default {
+export default [
+{
   input: 'src/main.ts',
   output: [
     {
@@ -34,4 +36,13 @@ export default {
       exclude: ['node_modules'],
     }),
   ]
-}
+},
+{
+    // path to your declaration files root
+    input: './src/main.d.ts',
+    output: [{ file: packageJson.typings, format: 'es' }],
+    plugins: [
+		svgr({typescript: true}),
+		dts()],
+  },
+]
